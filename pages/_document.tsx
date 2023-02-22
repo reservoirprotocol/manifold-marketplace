2da80a6460068ee5d969e9a1a9e6cb8b66a81344
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-02-22 11:50:35 -0500

    Update meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,10 +40,7 @@ class MyDocument extends Document {
 
         {/* CONFIGURABLE: Reservoir meta tags can be configured to your application, learn more here: https://docs.reservoir.tools/docs/reservoir-meta-tags */}
         <meta property="reservoir:title" content="Manifold" />
-        <meta
-          property="reservoir:icon"
-          content="https://manifold.xyz/assets/manifold-favicon.23612492.svg"
-        />
+        <meta property="reservoir:icon" content="/manifold-source-logo.png" />
         <meta
           property="reservoir:token-url-mainnet"
           content="/collection/ethereum/${contract}/${tokenId}"
