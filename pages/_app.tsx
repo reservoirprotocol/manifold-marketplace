9154f7948b883ad124733067476b4494ea1aa96d
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-03-15 13:58:15 -0400

    Resolve conflicts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -150,7 +150,6 @@ function MyApp({
           options={{
             //CONFIGURABLE: Override any configuration available in RK: https://docs.reservoir.tools/docs/reservoirkit-ui#configuring-reservoirkit-ui
             // Note that you should at the very least configure the source with your own domain
-<<<<<<< HEAD
             chains: supportedChains.map(({ proxyApi, id }) => {
               return {
                 id,
@@ -158,9 +157,6 @@ function MyApp({
                 default: marketplaceChain.id === id,
               }
             }),
-=======
-            apiBase: `${baseUrl}${marketplaceChain.proxyApi}`,
->>>>>>> 7cc5fe9 (Initial configuration)
             source: 'manifold.reservoir.market',
             normalizeRoyalties: NORMALIZE_ROYALTIES,
           }}
